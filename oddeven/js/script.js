a81a376f4fa6a9f28400f44046bc74c61d0908a6
Charlene Becker <ourschar@gmail.com>
2016-09-29 14:24:23 -0400

    Created Even/Odd App

---
+++ oddeven/js/script.js
@@ -0,0 +1,26 @@
+function onSubmit() {
+  var answerBox = document.getElementById("answerBox");
+  var boxValue = document.getElementById("textBox").value;
+
+  if (boxValue % 2 == 0) {
+    answerBox.innerHTML = "EVEN!";
+  }
+  else {
+    answerBox.innerHTML = "ODD!";
+  }
+  return;
+}
+
+function reset() {
+  var answerBox = document.getElementById("answerBox");
+  answerBox.innerHTML = "";
+  var textBox = document.getElementById("textBox");
+  textBox.value = "";
+
+  return;
+}
+
+//Don't need the function to fire-off on this line because _clicking_ the button is the catalyst. 
+
+
+
